dbcf805bddaf5a80485d88869a620c5aa192c642
Vinicius <vinicius.stmartins@gmail.com>
2022-10-18 14:47:41 -0300

    create : create Component Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,1 +1,19 @@
-<div> oi </div>
+const Button = ({
+    children,
+    backGround,
+    textColor,
+    ...rest
+  }) => {
+    return (
+      <button
+        type="button"
+        backGround={backGround}
+        textColor={textColor}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  };
+  export { Button };
+  

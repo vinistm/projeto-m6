2a759e0baea0cdd8b50db3ee51989bb5d396eab1
talitta-nunes <70520439+talitta-nunes@users.noreply.github.com>
2022-10-24 09:18:46 -0300

    fix: improving color

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -10,13 +10,14 @@ export const Container = styled.div`
   width: 250px;
   max-width: 100%;
   height: 300px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   color: var(--black);
 
   .card-container p {
     padding: 5px;
+    font-size: 16px;
   }
   .card-image img {
+    background-color: var(--grey-7);
     width: 312px;
     max-width: 100%;
     height: 153px;
@@ -40,5 +41,5 @@ export const MainContainer = styled.div`
   width: 1471px;
   max-width: 100%;
   height: 391px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  
 `;
